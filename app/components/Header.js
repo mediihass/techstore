ec255a89c89579620f5764ae4997cf81d8891896
mediihass <medii.hass@gmail.com>
2025-12-23 00:07:17 +0100

    snowfall

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,24 +4,34 @@ import Marquee from "react-fast-marquee";
 
 export default function Header() {
   return (
-    <header className="w-full  bg-gray-100 text-center flex flex-col items-center gap-2">
-      {/* Nom du site */}
-      <Image
-        src="/images/logo.svg"
-        alt="Logo"
-        width={200}
-        height={150}
-        className="object-contain"
-      />
+    <header className="w-full bg-gray-100 text-center flex flex-col items-center gap-2 ">
+      {/* Nom du site avec le bonnet de Noël */}
+      <div className="relative top-1">
+        <Image
+          src="/images/logo.svg"
+          alt="Logo"
+          width={200}
+          height={160}
+          className="object-contain"
+        />
+        <Image
+          src="/images/bonnet-noel.png"
+          alt="Bonnet de Noël"
+          width={50}
+          height={50}
+          className="absolute top-0 left-0  transform -translate-x-5 scale-x-[-1]"
+        />
+      </div>
 
       {/* Mini-slogan */}
       <p className="text-sm text-gray-600 font-poppins">
         High-Tech & Innovation
       </p>
+
       <Marquee
         speed={30}
         gradient={false}
-        className="bg-red-700  text-white font-bold py-3"
+        className="bg-red-700 text-white font-bold py-3"
       >
         🛍️ **Découvrez les dernières technologies !** ✨ **Ne manquez pas nos
         offres exclusives sur les gadgets et appareils électroniques !** 💻
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import BlurText from "../app/components/BlurText";
+import Snowfall from "react-snowfall";
 
 const handleAnimationComplete = () => {
   console.log("Animation completed!");
@@ -8,43 +9,46 @@ const handleAnimationComplete = () => {
 
 export default function HomePage() {
   return (
-    <main className="w-screen min-h-screen flex flex-col items-center justify-center">
-      <section className="w-screen relative">
-        <div className="h-[80vh]">
-          {/* Overlay */}
-          <div
-            className="absolute w-full bg-cover bg-center inset-0 bg-black bg-opacity-80 flex flex-col items-center  px-4"
-            style={{ backgroundImage: "url('/images/background.jpg')" }}
-          >
-            {/* Animated Heading */}
-            <BlurText
-              text="Bienvenue sur TechStore"
-              delay={200}
-              animateBy="words"
-              direction="top"
-              onAnimationComplete={handleAnimationComplete}
-              className="text-white text-4xl text-center flex justify-center md:text-6xl font-bold bg-red-900 rounded-xl mb-6 mt-16 p-2"
-            />
+    <>
+      <Snowfall color="#82c3d9" snowflakeCount={150} />
+      <main className="w-screen min-h-screen flex flex-col items-center justify-center">
+        <section className="w-screen relative">
+          <div className="h-[80vh]">
+            {/* Overlay */}
+            <div
+              className="absolute w-full bg-cover bg-center inset-0 bg-black bg-opacity-80 flex flex-col items-center  px-4"
+              style={{ backgroundImage: "url('/images/background.jpg')" }}
+            >
+              {/* Animated Heading */}
+              <BlurText
+                text="Bienvenue sur TechStore"
+                delay={200}
+                animateBy="words"
+                direction="top"
+                onAnimationComplete={handleAnimationComplete}
+                className="text-white text-4xl text-center flex justify-center md:text-6xl font-bold bg-red-900 rounded-xl mb-6 mt-16 p-2"
+              />
 
-            {/* Links */}
-            <div className="flex flex-col md:flex-row gap-4">
-              <Link
-                href="/produits"
-                className="bg-green-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-green-700 transition-colors duration-300"
-              >
-                Produits
-              </Link>
+              {/* Links */}
+              <div className="flex flex-col md:flex-row gap-4">
+                <Link
+                  href="/produits"
+                  className="bg-green-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-green-700 transition-colors duration-300"
+                >
+                  Produits
+                </Link>
 
-              <Link
-                href="/inscription"
-                className="bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-700 transition-colors duration-300"
-              >
-                S’inscrire
-              </Link>
+                <Link
+                  href="/inscription"
+                  className="bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-700 transition-colors duration-300"
+                >
+                  S’inscrire
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </>
   );
 }
